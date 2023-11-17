726068335a836d14b27e355bda90a7d83872a34e
leo <leonardo.foc.lav@gmail.com>
2023-11-17 16:27:47 +0100

    makeFS

--- samba_decepion_component_generator.py
+++ samba_decepion_component_generator.py
@@ -239,6 +239,7 @@ base_smb_config_content = """#======================= Global Settings ==========
 
 base_setup_content = """import subprocess
 import random
+import os
 
 def create_user(username, password):
     try:
@@ -248,6 +249,33 @@ def create_user(username, password):
         print(f'Utente "{username}" creato con successo.')
     except subprocess.CalledProcessError as e:
        print(f'Errore durante la creazione dell\\'utente: {e}')
+       
+def makeFS(type):
+    # questp è stato messo qui per creare la cartella della base path perchèà deve esistere, non è detto che serva dipende da dove condivide samba
+    # Specify the path for the new folder
+    base_path = '/path/to/your/base_folder'
+
+    # Use the os.makedirs() function to create the folder along with any necessary parent folders
+    os.makedirs(base_path)
+
+    print(f"Folder '{base_path}' created successfully.")
+    # TODO capire quel è la cartella dove samba condivide
+
+    # Specify the user folders
+    user_folders = ['Documents', 'Pictures', 'Downloads', 'Desktop']
+    for folder in user_folders:
+        folder_path = os.path.join(base_path, folder)
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created successfully.")
+    if type=="home":
+
+        # Create the full path by joining the base path with the nested folders
+        full_path = os.path.join(base_path, *user_folders)
+
+        # Use os.makedirs() to create the nested folder structure
+        os.makedirs(full_path)
+
+        print(f"Folder structure '{full_path}' created successfully.")
        
 def generate_random_sentence(num_words):
     words = [
--- utils/setup.py
+++ utils/setup.py
@@ -1,5 +1,6 @@
 import subprocess
 import random
+import os
 
 
 def create_user(username, password):
@@ -11,6 +12,32 @@ def create_user(username, password):
     except subprocess.CalledProcessError as e:
         print(f'Errore durante la creazione dell\'utente: {e}')
 
+def makeFS(type):
+    # questp è stato messo qui per creare la cartella della base path perchèà deve esistere, non è detto che serva dipende da dove condivide samba
+    # Specify the path for the new folder
+    base_path = '/path/to/your/base_folder'
+
+    # Use the os.makedirs() function to create the folder along with any necessary parent folders
+    os.makedirs(base_path)
+
+    print(f"Folder '{base_path}' created successfully.")
+    # TODO capire quel è la cartella dove samba condivide
+
+    # Specify the user folders
+    user_folders = ['Documents', 'Pictures', 'Downloads', 'Desktop']
+    for folder in user_folders:
+        folder_path = os.path.join(base_path, folder)
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created successfully.")
+    if type=="home":
+
+        # Create the full path by joining the base path with the nested folders
+        full_path = os.path.join(base_path, *user_folders)
+
+        # Use os.makedirs() to create the nested folder structure
+        os.makedirs(full_path)
+
+        print(f"Folder structure '{full_path}' created successfully.")
 
 def generate_random_sentence(num_words):
     words = [
