1e3161ba8c0fba9a0639b9c00fcb4d4e371ffd91
antcass <106447777+antcass@users.noreply.github.com>
2023-11-18 11:41:13 +0100

    modify samba configuration file

--- utils/setup.py
+++ utils/setup.py
@@ -2,21 +2,6 @@ import subprocess
 import random
 import os
 from datetime import datetime,timedelta
-import ldap
-
-def authenticate(username, password):
-    ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-    server = "ldaps://ldap.example.com:636"
-    base_dn = "dc=example.com"
-    user_dn = "uid={},{}".format(username, base_dn)
-    try:
-        l = ldap.initialize(server)
-        l.protocol_version = ldap.VERSION3
-
-        l.simple_bind_s(user_dn, password)
-    except subprocess.CalledProcessError as e:
-        print(f'Errore LDAP: {e}')
-
 
 def generate_random_sentence(num_words):
     words = [
